e9e5e9a5a78adb844fe637a750d48ed2ac56482e
YannickUllisch <yannicku01@gmail.com>
2025-07-12 08:52:40 +0200

    feat: repo deletion policy

--- modules/repository/ecr.tf
+++ modules/repository/ecr.tf
@@ -6,3 +6,25 @@ resource "aws_ecr_repository" "ecr" {
   }
 }
 
+# Pretty harsh lifecycle policy to ensure low cost and no untagged images
+resource "aws_ecr_lifecycle_policy" "remove_untagged" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 2
+        description  = "Remove untagged images after a day"
+        selection = {
+          tagStatus     = "untagged"
+          countType     = "sinceImagePushed"
+          countUnit     = "days"
+          countNumber   = 1
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
